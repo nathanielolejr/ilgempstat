91e2c852b5612b06b55fdbf9ad283a7ac0fafbaa
Sherwin Sandoval <33995402+eSmooothie@users.noreply.github.com>
2021-11-15 12:47:49 +0800

    set up choroplet visualization

--- map.js
+++ map.js
@@ -33,35 +33,17 @@ function addData(barangay, lng, lat){
   });
 }
 
-saveData.addEventListener('click',(e) => {
-  var barangay = document.getElementById('Barangay').value;
-  var longitude = document.getElementById('longitude').value;
-  var latitude = document.getElementById('latitude').value;
-  // 8.234175, 124.235162
-
-  addData(barangay, longitude, latitude);
-  
-  alert('saved');
-  
-});
-
 // get a reference of the table
 const dbRef = ref(db, 'Barangays/');
 
 // get all values
 onValue(dbRef,  function(snapshot){
   const data =  snapshot.val();
-  var locations = [];
-  // do something here
-  // for(const property in data){
-  //   // console.log(data[property]);
-  //   let lat = data[property]['lat'];
-  //   let lng = data[property]['lng'];
-  //   locations.push([lat, lng]);
-  // }
   // rebuild the map
-  buildMap(locations);
+  buildMap();
 });
+
+
 
 function buildMap(locations = []){
   // check if the map is already initiated or not
@@ -71,24 +53,16 @@ function buildMap(locations = []){
   }
   
   document.getElementById('map').innerHTML = "<div id='map'></div>";
-  var mymap = L.map('map').setView([8.229567, 124.245070], 14); 
+  var mymap = L.map('map').setView([8.229567, 124.245070], 12); 
 
   L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
-    zoom: 9,
-    maxZoom: 15,
+    maxZoom: 18,
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, ' +
     'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     id: 'mapbox/streets-v11',
     tileSize: 512,
   zoomOffset: -1
   }).addTo(mymap);
-
-  // update marker
-  for(const property in locations){
-    var latlng = locations[property];
-    console.log(latlng);
-    L.marker(latlng).addTo(mymap);
-  }
 
   // GeoJson
   
@@ -135,135 +109,158 @@ function buildMap(locations = []){
     601, 610,
   ];
 
-
   if(online){
     // Get a reference to the storage service, which is used to create references in your storage bucket
     const storage = getStorage();
     // console.log(storage);
-
+    
     // Create a reference from a Google Cloud Storage URI
-    const gsReference = storageRef(storage, 'gs://ilgempstat.appspot.com/PH10.json');
+    const gsReference = storageRef(storage, "gs://ilgempstat.appspot.com/PH10.json");
 
     getDownloadURL(gsReference)
     .then((data) => {
-      var geoJsonFilePath = data;
-  
-      var getGeoJson = $.getJSON(geoJsonFilePath, function(json) {
-        var geometries = json.geometries;
-        // console.log(geometries);
-    
-        // const geoDataLen = geometries.length;
-        // console.log(geoDataLen);
-  
-        for (let index = 0; index < ilg.length ; index++) {
-  
-          const i = ilg[index];
-  
-          const geo = geometries[i];
-          // console.log(geo);
-  
-          // GeoJson Feature
-          const geoJsonFeature = {
-            "type": "Feature",
-            "properties" :{
-              "name": index,
-              "popupContent": ""+index
-            },
-            "geometry" : geo,
-          }
-    
-          const defaultStyle = {
-            color: "#808080",
-            fill: true,
-          }
-    
-          L.geoJSON(geoJsonFeature, {
-            style: defaultStyle
-            })
-            .bindPopup(function (layer){
-              return layer.feature.properties.popupContent;
-            })
-            .addTo(mymap);
-        }
-      });
+      showGeoJson(mymap, ilg, data);
     })
     .catch((error) => {
       // Handle any errors
       console.log(error);
     });
-  
   }else{
-    var geoJsonFilePath = "PH10/PH10.json";
-  
-      var getGeoJson = $.getJSON(geoJsonFilePath, function(json) {
-        var geometries = json.geometries;
-        // console.log(geometries);
-    
-       
-        // const geoDataLen = geometries.length;
-        // console.log(geoDataLen);
-  
-        for (let index = 0; index < ilg.length ; index++) {
-  
-          const i = ilg[index];
-  
-          const geo = geometries[i];
-          // console.log(geo);
-  
-          // GeoJson Feature
-          const geoJsonFeature = {
-            "type": "Feature",
-            "properties" :{
-              "name": index,
-              "popupContent": ""+index
-            },
-            "geometry" : geo,
-          }
-    
-          const defaultStyle = {
-            color: "#808080",
-            fill: true,
-          }
-    
-          L.geoJSON(geoJsonFeature, {
-            style: defaultStyle
-            })
-            .bindPopup(function (layer){
-              return layer.feature.properties.popupContent;
-            })
-            .addTo(mymap);
+    showGeoJson(mymap, ilg);
+  } 
+}
+
+function getColor(value) {
+  return value > 1000 ? '#800026' :
+         value > 500  ? '#BD0026' :
+         value > 200  ? '#E31A1C' :
+         value > 100  ? '#FC4E2A' :
+         value > 50   ? '#FD8D3C' :
+         value > 20   ? '#FEB24C' :
+         value > 10   ? '#FED976' :
+                    '#FFEDA0';
+}
+
+function style(feature) {
+  return {
+      fillColor: getColor(feature.properties.employment),
+      weight: 2,
+      opacity: 1,
+      color: 'white',
+      dashArray: '3',
+      fillOpacity: 0.7
+  };
+}
+
+
+function showGeoJson(map, iligan, geoJsonFile = "PH10/PH10.json"){
+  var getGeoJson = $.getJSON(geoJsonFile, function(json) {
+    var geometries = json.geometries;
+
+    // custom info window
+    var info = L.control();
+
+    info.onAdd = function (map) {
+      this._div = L.DomUtil.create('div', 'info'); // create a div with a class "info"
+      this.update();
+      return this._div;
+    };
+
+    // method that we will use to update the control based on feature properties 
+    // passed
+    info.update = function (props) {
+      this._div.innerHTML = '<h4>Employment Status Per Barangay in Iligan City</h4>' +  (props ?
+          '<b>' + props.name + '</b><br />' + props.employment + ' employed / mi<sup>2</sup>'
+          : 'Hover over a state');
+    };
+
+    info.addTo(map);
+
+    // custom legend
+    var legend = L.control({position: 'bottomright'});
+
+    legend.onAdd = function (map) {
+
+        var div = L.DomUtil.create('div', 'info legend'),
+            grades = [0, 10, 20, 50, 100, 200, 500, 1000],
+            labels = [];
+
+        // loop through our density intervals and generate a label with a colored square for each interval
+        for (var i = 0; i < grades.length; i++) {
+            div.innerHTML +=
+                '<i style="background:' + getColor(grades[i] + 1) + '"></i> ' +
+                grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
         }
-      });
-  }
 
-  
- 
-  
-  
+        return div;
+    };
 
-  
+    legend.addTo(map);
 
- 
+    for (let index = 0; index < iligan.length ; index++) {
+
+      const i = iligan[index];
+
+      const geo = geometries[i];
+
+      // GeoJson Feature
+      const geoJsonFeature = {
+        "type": "Feature",
+        "properties" :{
+          "name": i,
+          "employment": Math.floor(Math.random() * 1000),
+        },
+        "geometry" : geo,
+      }
+
+      const defaultStyle = {
+        color: "#808080",
+        fill: true,
+      }
+
+      
+
+      var geoJson = L.geoJSON(geoJsonFeature, {
+        style: style,
+        onEachFeature: function(feature, layer){
+          layer.on({
+            mouseover: function(e){
+              var layer = e.target;
+              layer.setStyle({
+                  weight: 5,
+                  color: "#316B83",
+                  dashArray: '',
+                  fillOpacity: 0.7
+              });
+
+              info.update(layer.feature.properties);
+
+              if (!L.Browser.ie && !L.Browser.opera && !L.Browser.edge) {
+                  layer.bringToFront();
+              }
+            },
+            mouseout: function(e){
+              geoJson.resetStyle(e.target);
+              info.update();
+            },
+            click: function(e){
+              map.fitBounds(e.target.getBounds());
+            }
+          });
+        },
+        }) 
+        .addTo(map);
+
+
+
+
+    }
+  });
 }
 
 
 
-/**
- * 581
- * 607
- * 563
- * 603
- * 588
- * 673
- * 579
- * 597
- * 595
- * 596
- * 598
- * 593
- * 585
- * 614
- */
+
 
 
 
